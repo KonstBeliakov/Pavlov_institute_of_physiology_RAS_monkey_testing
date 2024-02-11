8ffc20bcbe3246d99d82af2d5da79e9475edb1cc
KonstBeliakov <konstbeliakov@gmail.com>
2024-02-12 02:42:44 +0300

    Добавлена вкладка запуск, "Запуск", кнопка "Запустить тестирование" перемещена в эту вкладку

--- main.py
+++ main.py
@@ -25,21 +25,20 @@ class App(tk.Tk):
         super().__init__()
         notebook = ttk.Notebook()
         notebook.pack(expand=True, fill=BOTH)
-        frame_text = ['Информация о приложении', 'Настройки тестирования А', 'Настройки тестирования B', 'Настройки тестирования C']
-        frame = [ttk.Frame(notebook) for _ in range(2)]
+        frame_text = ['Информация о приложении', 'Настройки тестирования А', 'Настройки тестирования B',
+                      'Настройки тестирования C', 'Запуск']
+        frame = [ttk.Frame(notebook) for _ in range(5)]
 
         for i in range(len(frame)):
             frame[i].pack(fill=BOTH, expand=True)
 
-        self.test_image = [PhotoImage(file="info.png"), PhotoImage(file="settings.png")]
+        self.test_image = [PhotoImage(file="info.png"), PhotoImage(file="settings.png"), PhotoImage(file="settings.png"), PhotoImage(file="settings.png"),
+                           PhotoImage(file="run.png")]
 
         for i in range(len(frame)):
             notebook.add(frame[i], text=frame_text[i], image=self.test_image[i], compound=LEFT)
 
-        self.label = tk.Label(frame[1], text='Это окно для настройки параметров тестирования')
-        self.btn = tk.Button(frame[1], text="Запустить тестирование",
-                             command=self.open_about)
-        self.label.pack()
+        self.btn = tk.Button(frame[-1], text="Запустить тестирование", command=self.open_about)
         self.btn.pack(padx=50, pady=20)
 
     def open_about(self):
