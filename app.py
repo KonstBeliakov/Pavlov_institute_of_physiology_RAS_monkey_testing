ce0ac36a816734a76b6cf2269978b991e944bc36
KonstBeliakov <konstbeliakov@gmail.com>
2024-02-13 20:15:13 +0300

    Дополнен интерфейс вкладки запуска Созданы радиокнопки для обнуления таймера Добавлен выпадающий список для выбора типа эксперимента

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import time
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 from monkey_window import Monkey_window
+import threading
 
 import settings
 
@@ -9,6 +11,13 @@ import settings
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.timer_ask_label = None
+        self.choose_experiment_label = None
+        self.choose_experiment_combobox = None
+        self.radio_button_no = None
+        self.radio_button_yes = None
+        self.timer_radio_buttons = None
+        self.update_thread = None
         self.log_label = []
         self.btn_confirm = None
         self.error_label = None
@@ -66,15 +75,34 @@ class App(tk.Tk):
 
     def run_frame_init(self):
         frame_number = 1
+        self.choose_experiment_label = Label(self.frame[frame_number], text='Выберите тип эксперимента из списка доступных')
+        self.choose_experiment_label.grid(row=0, column=0)
+
+        self.choose_experiment_combobox = ttk.Combobox(self.frame[frame_number], values=['Запоминание картинки'])
+        self.choose_experiment_combobox.grid(row=1, column=0)
+
+        self.timer_ask_label = Label(self.frame[frame_number], text='Обнулить глобальный таймер эксперимента?')
+        self.timer_ask_label.grid(row=2, column=0)
+
+        self.timer_radio_buttons = IntVar()
+        self.timer_radio_buttons.set(0)
+        self.radio_button_yes = Radiobutton(self.frame[frame_number], text="Да", variable=self.timer_radio_buttons, value=1)
+        self.radio_button_no = Radiobutton(self.frame[frame_number], text="Нет", variable=self.timer_radio_buttons, value=0)
+        self.radio_button_yes.grid(row=3, column=0)
+        self.radio_button_no.grid(row=3, column=1)
+
         self.btn = tk.Button(self.frame[frame_number], text="Запустить тестирование", command=self.open_about)
-        self.btn.grid(row=0, column=0)
+        self.btn.grid(row=4, column=0)
 
     def open_about(self):
         if not self.started:
             self.btn.configure(text="Завершить тестирование")
             self.started = True
             self.window = Monkey_window()
-            self.update_log()
+
+            self.update_thread = threading.Thread(target=self.update_log)
+            self.update_thread.start()
+
             self.window.mainloop()
         else:
             self.window.destroy()
@@ -91,9 +119,12 @@ class App(tk.Tk):
             self.error_label.grid_forget()
 
     def update_log(self):
-        self.log_label = [[tk.Label(self.frame[1], text=text, fg='#0f0' if line[-1] == line[-2] else '#f00')
-                           for text in line] for line in self.window.log[-10:]]
-        for i in range(len(self.log_label)):
-            for j in range(len(self.log_label[i])):
-                self.log_label[i][j].grid(row=i, column=1 + j)
-        self.after(1000, self.update_log)
+        while True:
+            print('updating log...', end='')
+            self.log_label = [[tk.Label(self.frame[1], text=text, fg='#0f0' if line[-1] == line[-2] else '#f00')
+                               for text in line] for line in self.window.log[-10:]]
+            for i in range(len(self.log_label)):
+                for j in range(len(self.log_label[i])):
+                    self.log_label[i][j].grid(row=i, column=1 + j)
+            print('done')
+            time.sleep(1)
