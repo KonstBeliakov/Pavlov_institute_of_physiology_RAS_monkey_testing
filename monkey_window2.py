9fea83e6ba3f3aae3b13458b28179031f51e7b5b
KonstBeliakov <konstbeliakov@gmail.com>
2024-03-10 00:14:47 +0300

    Исправления в эксперименте 2 рандомизирована скорость, движение стало плавным

--- monkey_window2.py
+++ monkey_window2.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import threading
 from time import sleep, perf_counter
 from tkinter import *
+from settings import *
+from random import randint
 
 
 class MonkeyWindow2(tk.Toplevel):
@@ -12,14 +14,14 @@ class MonkeyWindow2(tk.Toplevel):
         self.title('Experiment window')
         self.geometry('800x800')
         self.image_position = [10, 50]
-        self.image_speed = 10
-
-        self.canvas = Canvas(self, bg="white", width=500, height=500)
+        self.image_speed = randint(image_min_speed, image_max_speed)
+        self.canvas_size = [500, 500]
+        self.canvas = Canvas(self, bg="white", width=self.canvas_size[0], height=self.canvas_size[1])
         self.canvas.pack(anchor=CENTER, expand=1)
 
         self.python_image = PhotoImage(file="settings.png")
 
-        self.canvas.create_image(*self.image_position, image=self.python_image)
+        self.image = self.canvas.create_image(*self.image_position, image=self.python_image)
 
         self.t1 = threading.Thread(target=self.update)
         self.time = perf_counter()
@@ -27,12 +29,9 @@ class MonkeyWindow2(tk.Toplevel):
 
     def update(self):
         while True:
-            self.image_position = [self.image_position[0] + self.image_speed * (perf_counter() - self.time),
-                                   self.image_position[1]]
-            self.canvas.delete("all")
-            self.canvas.create_image(*self.image_position, image=self.python_image)
+            self.canvas.move(self.image, self.image_speed * (perf_counter() - self.time), 0)
             self.time = perf_counter()
-            sleep(0.1)
+            sleep(0.01)
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,5 +1,10 @@
+# First type experiment settings
 delay = [1.0, 1.0, 1.0, 0.0, 5.0]
 session_number = 1
 repeat_number = 5
 experiment_start = None
 restart_after_answer = False
+
+# Second type experiment settings
+image_min_speed = 10
+image_max_speed = 50
