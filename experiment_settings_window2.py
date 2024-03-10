655ff94692485a220695d2f81e426a14e9338afa
KonstBeliakov <konstbeliakov@gmail.com>
2024-03-10 23:00:06 +0300

    Код окна настроек эксперимента 2 сокращен

--- experiment_settings_window2.py
+++ experiment_settings_window2.py
@@ -12,11 +12,11 @@ class ExperimentSettingsWindow2(SettingsWindow):
         self.settingsFrame = Frame(self)
         self.settingsFrame.grid(column=0, row=0)
 
-        self.labels = [Label(self.settingsFrame, text=text) for text in ['Минимальная скорость изображения',
-                                                          'Максимальная скорость изображения',
-                                                          'Ширина барьера', 'Цвет барьера', 'Количество изображений',
-                                                          'Расстояние до барьера', 'Прямое движение',
-                                                           'Задержка между экспериментами', 'Число повторений']]
+        label_names = ['Минимальная скорость изображения', 'Максимальная скорость изображения', 'Ширина барьера',
+                       'Цвет барьера', 'Количество изображений', 'Расстояние до барьера', 'Прямое движение',
+                       'Задержка между экспериментами', 'Число повторений']
+
+        self.labels = [Label(self.settingsFrame, text=text) for text in label_names]
 
         self.min_speed_entry = Entry(self.settingsFrame)
         self.min_speed_entry.grid(column=1, row=0)
@@ -53,83 +53,43 @@ class ExperimentSettingsWindow2(SettingsWindow):
         for i, label in enumerate(self.labels):
             label.grid(column=0, row=i)
 
+    def int_value_check(self, value, name, declension=1, min_value=None, max_value=None):
+        if not value:
+            return f'{name} не указан{["", "о", "а"][declension]}'
+        try:
+            int(value)
+        except:
+            return f'{name} долж{["ен", "но", "на"][declension]} быть целым числом'
+        if min_value is not None and min_value > int(value):
+            return f'{name} не может быть меньше {min_value}'
+        if max_value is not None and max_value < int(value):
+            return f'{name} не может быть больше {max_value}'
+
     def save_settings(self):
-        self.error_label.grid_forget()
-        error_text = ''
-
-        if not self.min_speed_entry.get():
-            error_text = 'Значение минимальной скорости не указано'
-        if not error_text and not self.max_speed_entry.get():
-            error_text = 'Значение максимальной скорости не указано'
-
+        error_text = self.int_value_check(self.min_speed_entry.get(),
+                                          'Минимальное значение скорости', declension=1, min_value=0)
         if not error_text:
-            try:
-                int(self.min_speed_entry.get())
-                int(self.max_speed_entry.get())
-            except:
-                error_text = 'Значения минимальной и максимальной скоростей изображения должны быть целыми числами'
-
-        if not error_text and int(self.min_speed_entry.get()) < 0 or int(self.max_speed_entry.get()) < 0:
-            error_text = 'Значения скоростей не могут быть отрицательными'
+            error_text = self.int_value_check(self.max_speed_entry.get(),
+                                              'Максимальное значение скорости', declension=1, min_value=0)
 
         if not error_text and int(self.min_speed_entry.get()) > int(self.max_speed_entry.get()):
             error_text = 'Максимальное значение скорости должно быть больше минимального'
 
-        if not error_text and not self.barrier_width_entry.get():
-            error_text = 'Ширина барьера не указана'
-
         if not error_text:
-            try:
-                assert int(self.barrier_width_entry.get()) > 0
-            except:
-                error_text = 'Ширина барьера должна быть положительным числом'
-
-        if not error_text and not self.image_number_entry.get():
-            error_text = 'Количество изображений не указано'
-
+            error_text = self.int_value_check(self.barrier_width_entry.get(),
+                                              'Ширина барьера', declension=2, min_value=0)
         if not error_text:
-            try:
-                int(self.image_number_entry.get())
-            except:
-                error_text = 'Количество изображений должно быть целым числом'
-
+            error_text = self.int_value_check(self.image_number_entry.get(),
+                                              'Количество изображений', declension=1, min_value=1, max_value=10)
         if not error_text:
-            if not 10 >= int(self.image_number_entry.get()) > 0:
-                error_text = 'Количество изображений не может быть меньше 1 или больше 10'
-
-        if not error_text and not self.barrier_dist_entry.get():
-            error_text = 'Расстояние до барьера не указано'
-
+            error_text = self.int_value_check(self.barrier_dist_entry.get(),
+                                              'Расстояние до барьера', declension=1, min_value=0)
         if not error_text:
-            try:
-                int(self.barrier_dist_entry.get())
-            except:
-                error_text = 'Расстояние до барьера должно быть целым числом'
-
-        if not error_text and int(self.barrier_dist_entry.get()) < 0:
-            error_text = 'Расстояние до барьера не может быть отрицательным'
-
-        if not error_text and not self.session_delay_entry.get():
-            error_text = 'Задержка между экспериментами не указана'
-
+            error_text = self.int_value_check(self.session_delay_entry.get(),
+                                              'Задержка между экспериментами', declension=2, min_value=0)
         if not error_text:
-            try:
-                float(self.session_delay_entry.get())
-            except:
-                error_text = 'Задержка между экспериментами должна быть вещественным числом'
-
-        if not error_text and float(self.session_delay_entry.get()) < 0:
-            error_text = 'Задержка между экспериментами должна быть положительной'
-
-        if not error_text and not self.repeat_entry.get():
-            error_text = 'Количество повторений не задано'
-        if not error_text:
-            try:
-                int(self.repeat_entry.get())
-            except:
-                error_text = 'Количество повторений должно быть целым числом'
-        if not error_text and int(self.repeat_entry.get()) < 0:
-            error_text = 'Количество повторений должно быть положительным'
+            error_text = self.int_value_check(self.repeat_entry.get(),
+                                              'Количество повторений', declension=1, min_value=0)
 
         if error_text:
             self.error_label.configure(text=error_text)
