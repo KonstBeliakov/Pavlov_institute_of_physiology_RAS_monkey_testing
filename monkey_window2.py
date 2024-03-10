8f7dc03444326dc55045da7fb92e1f4492035115
KonstBeliakov <konstbeliakov@gmail.com>
2024-03-10 13:00:30 +0300

    Эксперимент 2 Вспомогательные изображения появляются только после пересечения ими барьера

--- monkey_window2.py
+++ monkey_window2.py
@@ -3,7 +3,7 @@ import threading
 from time import sleep, perf_counter
 from tkinter import *
 from settings import *
-from random import randint
+from random import randint, randrange
 
 
 class MonkeyWindow2(tk.Toplevel):
@@ -25,7 +25,13 @@ class MonkeyWindow2(tk.Toplevel):
 
         self.python_image = PhotoImage(file="settings.png")
 
-        self.image = [self.canvas.create_image(*self.image_position[i], image=self.python_image) for i in range(image_number)]
+        self.image = [self.canvas.create_image(*self.image_position[i], image=self.python_image) for i in
+                      range(image_number)]
+        self.right_image = randrange(image_number)
+        self.draw = [i == self.right_image for i in range(image_number)]
+        for i in range(image_number):
+            if i != self.right_image:
+                self.canvas.itemconfig(self.image[i], state='hidden')
         self.barrier = self.canvas.create_rectangle((self.canvas_size[0] - barrier_width) // 2, 0,
                                                     barrier_width + (self.canvas_size[0] - barrier_width) // 2,
                                                     self.canvas_size[1], fill=barrier_color)
@@ -42,7 +48,22 @@ class MonkeyWindow2(tk.Toplevel):
             for i in range(image_number):
                 self.canvas.move(self.image[i], self.image_speed * (perf_counter() - self.time[i]), 0)
                 self.time[i] = perf_counter()
+
+                if self.is_image_behind_barrier(i):
+                    self.canvas.itemconfig(self.image[i], state='normal')
             sleep(0.05)
+
+    def is_image_behind_barrier(self, n: int):
+        img_pos = [self.canvas.coords(self.image[n])[0] - self.image_size // 2,
+                   self.canvas.coords(self.image[n])[1] + self.image_size // 2]
+        barrier_pos = ((self.canvas_size[0] - barrier_width) // 2, 0,
+                       barrier_width + (self.canvas_size[0] - barrier_width) // 2, self.canvas_size[1])
+        t = [img_pos, [img_pos[0] + self.image_size, img_pos[1]], [img_pos[0], img_pos[1] + self.image_size],
+             [img_pos[0] + self.image_size, img_pos[1] + self.image_size]]
+        t2 = 0
+        for i in t:
+            t2 += (barrier_pos[0] < i[0] < barrier_pos[2] and barrier_pos[1] < i[1] < barrier_pos[3])
+        return t2 == 4
 
 
 if __name__ == '__main__':
