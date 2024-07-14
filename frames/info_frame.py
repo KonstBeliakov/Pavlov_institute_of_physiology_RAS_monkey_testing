d67a5980d7757d7bcb90166f3b77880b4c5a695b
KonstBeliakov <konstbeliakov@gmail.com>
2024-07-14 13:39:19 +0300

    Revert "index.html перенесен"
    This reverts commit a5490b1ed2a5111d14293229baf13a2f9008a7e9.

--- frames/info_frame.py
+++ frames/info_frame.py
@@ -7,7 +7,7 @@ class InfoFrame:
     def __init__(self, root):
         self.button = Button(root, text='Открыть документацию в браузере', command=self.open_documentation)
         self.button.pack()
-        self.doc_path = path.abspath(path.join('', 'index.html'))
+        self.doc_path = path.abspath(path.join('docs', 'index.html'))
 
         with open(self.doc_path, 'r', encoding='utf-8') as file:
             self.info_label = HTMLLabel(root, html=file.read())
