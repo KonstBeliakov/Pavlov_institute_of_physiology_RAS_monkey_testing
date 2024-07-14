8f1d265b4687aa26eec2886fccd9f873ace2b466
KonstBeliakov <konstbeliakov@gmail.com>
2024-07-14 11:56:13 +0300

    Улучшена документация, добавлен css, исправлена ошибка открытия документации из приложения

--- frames/info_frame.py
+++ frames/info_frame.py
@@ -1,16 +1,17 @@
 from tkhtmlview import HTMLLabel
 from tkinter import Button
-from os import startfile
+from os import startfile, path
 
 
 class InfoFrame:
     def __init__(self, root):
         self.button = Button(root, text='Открыть документацию в браузере', command=self.open_documentation)
         self.button.pack()
+        self.doc_path = path.abspath(path.join('docs', 'index.html'))
 
-        with open('info_frame_text.html', 'r', encoding='utf-8') as file:
+        with open(self.doc_path, 'r', encoding='utf-8') as file:
             self.info_label = HTMLLabel(root, html=file.read())
         self.info_label.pack(fill="both", expand=True)
 
     def open_documentation(self):
-        startfile('info_frame_text.html')
+        startfile(self.doc_path)
