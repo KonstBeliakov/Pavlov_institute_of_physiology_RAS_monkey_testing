64d671566db3c9e694eb4ac03c01a2958d2ef90f
KonstBeliakov <konstbeliakov@gmail.com>
2025-02-11 04:23:40 +0300

    Добавлен текст, показывающий подключена ли управляющая плата

--- frames/devise_check_frame.py
+++ frames/devise_check_frame.py
@@ -22,9 +22,12 @@ class DeviseCheckFrame:
         self.arduino_frame_header = CTkLabel(master=self.arduino_frame, text='Проверка управляющей платы')
         self.arduino_frame_header.grid(row=0, column=0)
 
+        self.port_available_label = CTkLabel(master=self.arduino_frame, text='Управляющая плата не подключена', text_color="#f66")
+        self.port_available_label.grid(row=1, column=0)
+
         self.check_devise_button = CTkButton(self.arduino_frame, text='Подключиться к управляющей плате',
                                              command=self.devise_check)
-        self.check_devise_button.grid(row=1, column=0, padx=4, pady=2)
+        self.check_devise_button.grid(row=2, column=0, padx=4, pady=2)
 
         self.button_check = [
             CTkButton(self.arduino_frame, text='Проверить поилку', command=utils.positive_reinforcement),
@@ -33,7 +36,7 @@ class DeviseCheckFrame:
         ]
 
         for i, button in enumerate(self.button_check):
-            button.grid(row=i + 2, column=0, padx=4, pady=2)
+            button.grid(row=i + 3, column=0, padx=4, pady=2)
 
         # -------------------- SECOND SCREEN FRAME --------------------
 
@@ -57,6 +60,8 @@ class DeviseCheckFrame:
         self.devise_check_error_label = CTkLabel(root, text='', text_color='red')
         self.devise_check_error_label.grid(row=1, column=0)
 
+        self.devise_check()
+
     def devise_check(self):
         if (error_text := self.devise_check_entries.save_values(check_validity=True)) is not None:
             self.devise_check_error_label.configure(text=error_text)
@@ -65,9 +70,9 @@ class DeviseCheckFrame:
             utils.check_serial()
 
             if utils.serial_available:
-                self.app.test_image[self.frame_number].configure(file='pictograms/yes.png')
+                self.port_available_label.configure(text="Управляющая плата подключена", text_color="#6f6")
             else:
-                self.app.test_image[self.frame_number].configure(file='pictograms/no.png')
+                self.port_available_label.configure(text="Управляющая плата не подключена", text_color="#f66")
 
     def check_second_screen(self):
         self.second_screen_copy = SecondScreenCopy(self.second_screen_frame, 0, 3)
